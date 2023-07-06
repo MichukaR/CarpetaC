9cebf86895f4e25ecc0792b4c955f5678a0872d3
Michelle Reyes <michelle.reyes.delgado@udla.edu.ec>
2023-07-05 21:41:37 -0500

    Pares Primos 1.1

--- Arreglos.c
+++ Arreglos.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
 int main (void) {
-    printf("Hola mundo");
+    int vector [5];
+    int vector2 [5];
+    int sumavec [5];
+    for (int i = 0; i < 5; i++)
+    {
+        printf("Ingrese el vector %d: " , i+1);
+        scanf("%d", &vector[i]);
+    }
+    for (int i = 4; i > 0; i--)
+    {
+        printf("Ingrese el vector numero 2 %d: " , i+1);
+        scanf("%d", &vector2[i]);
+    }
+    printf ("Los datos son :\n");
+    for (int i = 0; i < 5 ; i++)
+    {
+        sumavec [i] = vector [i] + vector2 [i];
+        printf("la %d suma es : %d\n", i+1 ,sumavec[i]);
+    }
+    
     return 0;
 }
