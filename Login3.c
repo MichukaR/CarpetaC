2ab99854a7c43b2861e21628ae2f958472c30088
Michelle Reyes <michelle.reyes.delgado@udla.edu.ec>
2023-07-12 22:07:49 -0500

    First commit

--- Login3.c
+++ Login3.c
@@ -20,42 +20,59 @@ int main (void){
         printf("Ingrese la contraseña\n");
         scanf("%s", pass);
         if (strcmp(usuario, usuario1)==0 && strcmp(pass, pass1)==0){
-            printf("Respuesta correcta\n");
+            printf("- Log in satisfactorio\n");
             break;
         }
         if (strcmp(usuario, usuario2)==0 && strcmp(pass, pass2)==0){
-            printf("Respuesta correcta\n");
+            printf("- Log in satisfacotrio\n");
             break;
         }
         if (strcmp(usuario, usuario3)==0 && strcmp(pass, pass3)==0){
-            printf("Respuesta correcta\n");
+            printf("- Log in satisfactorio\n");
             break;
         }
         n2--;
-        printf("Usuario o contraseña incorrectos\n");
-        printf("Le quedan %d intentos\n" , n2);
+        printf("- Usuario o contraseña incorrectos\n");
+        printf("- Le quedan %d intentos\n" , n2);
     } while (n2>0);
     if (n2==3)
     {
-        printf ("Numero de intentos excedido\n");
+        printf ("- Numero de intentos excedido\n");
     } else {
-        printf ("Que accion desea realizar?\n");
-        printf ("1= Hacer compra de boletos  2= Ver las estadisticas  3= Salir\n");
-        scanf ("%d", &opcion);
-        switch (opcion)
-        {
-        case 1:
-            printf("\n> Compra de boletos\n");
-            break;
-        case 2:
-            printf("\nEstadisticas\n");
-            break;
-        case 3:
-            break;
-        
-        default:
-            break;
-        }
+        do {
+            printf ("Que accion desea realizar?\n");
+            printf ("1= Hacer compra de boletos  2= Ver las estadisticas  3= Salir\n");
+            scanf ("%d", &opcion);
+            switch (opcion)
+            {
+            case 1:
+                printf("\n> Compra de boletos\n");
+                do{
+                    printf("Escoja la pelicula que desea ver siendo\n");
+                    printf("1= Superman   2=  Batman\n");
+                    scanf ("%d", &opcion2);
+                    switch (opcion2)
+                    {
+                    case 1:
+                        /* code */
+                        break;
+                    
+                    default:
+                        break;
+                    }
+                } while (opcion > 3);
+                break;
+            case 2:
+                printf("\nEstadisticas\n");
+                break;
+            case 3:
+                break;
+            
+            default:
+                printf("\nOpcion no valida ingrese de nuevo\n");
+                break;
+            }
+        } while (opcion!=3);
     }
     printf("FIN DEL PROGRAMA...");
     return 0;
